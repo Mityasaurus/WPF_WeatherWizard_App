a75de500b43ec8df54b066f73c83a68d09ccde57
Refer3n <refer3n@gmail.com>
2023-10-10 16:25:06 +0300

    Weather models added

---
+++ WPF_WeatherWizard_App/APP/Models/Day.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WPF_WeatherWizard_App.APP.Models
+{
+    public class Day
+    {
+        public DateTime Date { get; set; }
+
+        public decimal? MaxTempC { get; set; }
+
+        public decimal? MinTempC { get; set; }
+
+        public decimal? MaxTempF { get; set; }
+
+        public decimal? MinTempF { get; set; }
+
+        public string? Condition { get; set; }
+
+        public List<Hour>? Hours { get; set; }
+
+    }
+}
---
+++ WPF_WeatherWizard_App/APP/Models/Location.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WPF_WeatherWizard_App.APP.Models
+{
+    public class Location
+    {
+        public string Name { get; set; }
+
+        public string? Country { get; set; }
+
+        public DateTime? LocalTime { get; set; }
+    }
+}
