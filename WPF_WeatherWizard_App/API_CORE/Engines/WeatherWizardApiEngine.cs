74ebe22fab98553a8ec4b191ae6b16fabb458f7b
YuraSokolovskyi <sokolovskiiyura@gmail.com>
2023-10-09 20:33:00 -0400

    Added API engine class and interface.

--- WPF_WeatherWizard_App/API_CORE/Engines/WeatherWizardApiEngine.cs
+++ WPF_WeatherWizard_App/API_CORE/Engines/WeatherWizardApiEngine.cs
@@ -6,7 +6,7 @@ namespace WPF_WeatherWizard_App.API_CORE.Engines;
 public class WeatherWizardApiEngine : IApi
 {
     private HttpClient _client { get; set; }
-    private string _baseAddress = "http://api.weatherapi.com/v1/forecast.json";
+    private string _baseAddress = "http://api.weatherapi.com/v1/forecast.json?key=3e21b608b818455ea86165609230810";
     private string _apiKey = "3e21b608b818455ea86165609230810";
 
     public WeatherWizardApiEngine()
@@ -19,7 +19,7 @@ public class WeatherWizardApiEngine : IApi
     {
         var response = _client.SendAsync(
             new HttpRequestMessage(HttpMethod.Get, 
-                _baseAddress + $"?key={_apiKey}&q={city}&days={days}&aqi={aqi}&alerts={alerts}"
+                _baseAddress + $"&q={city}&days={days}&aqi={aqi}&alerts={alerts}"
                 ));
         var content = response.Result.Content.ReadAsStringAsync();
         return content.Result;
