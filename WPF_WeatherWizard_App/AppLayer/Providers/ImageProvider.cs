89b1082d30554ac833bf668d0f92df41ca84ef4a
Refer3n <refer3n@gmail.com>
2023-10-13 21:36:16 +0300

    ImageProvider and WeatherIconsConst added. MainWindow.xaml.cs and IconProvider updated.

---
+++ WPF_WeatherWizard_App/AppLayer/Providers/ImageProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace WPF_WeatherWizard_App.AppLayer.Providers
+{
+    public static class ImageProvider
+    {
+        public static void SetImageSource(Image image, string iconName)
+        {
+            try
+            {
+                image.Source = new BitmapImage(IconProvider.GetWeatherIconUri(iconName));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
